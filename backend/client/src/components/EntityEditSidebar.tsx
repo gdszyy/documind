24a3fc47cbfd530f01020b2dd9e4342132f28ad4
gdszyy <41259084+gdszyy@users.noreply.github.com>
2025-12-23 16:39:21 +0800

    Update EntityEditSidebar.tsx

--- backend/client/src/components/EntityEditSidebar.tsx
+++ backend/client/src/components/EntityEditSidebar.tsx
@@ -2,7 +2,6 @@ import { useAuth } from "@/_core/hooks/useAuth";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { Link } from "wouter";
 
 import {
   Select,
